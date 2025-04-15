ebc5fdce145c404af88ba83325f298b7cacb28df
mick <mtcoughlan@gmail.com>
2025-04-15 08:26:59 +0200

    Reset migrations and database, fresh start with working schema

--- migrations/env.py
+++ migrations/env.py
@@ -1,8 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from app import app
-
 import logging
 from logging.config import fileConfig
 
@@ -41,7 +36,8 @@ def get_engine_url():
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-
+config.set_main_option('sqlalchemy.url', get_engine_url())
+target_db = current_app.extensions['migrate'].db
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -50,7 +46,6 @@ def get_engine_url():
 
 
 def get_metadata():
-    target_db = current_app.extensions['migrate'].db
     if hasattr(target_db, 'metadatas'):
         return target_db.metadatas[None]
     return target_db.metadata
@@ -72,6 +67,7 @@ def run_migrations_offline():
     context.configure(
         url=url, target_metadata=get_metadata(), literal_binds=True
     )
+
     with context.begin_transaction():
         context.run_migrations()
 
@@ -97,19 +93,21 @@ def run_migrations_online():
     conf_args = current_app.extensions['migrate'].configure_args
     if conf_args.get("process_revision_directives") is None:
         conf_args["process_revision_directives"] = process_revision_directives
+
     connectable = get_engine()
+
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
             target_metadata=get_metadata(),
             **conf_args
         )
+
         with context.begin_transaction():
             context.run_migrations()
+
 
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-    with app.app_context():
-        config.set_main_option('sqlalchemy.url', get_engine_url())
-        run_migrations_online()
+    run_migrations_online()
